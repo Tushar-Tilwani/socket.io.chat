6388f348775f447f537cddb975aae7bdb900b06d
tushar <tushar.tilwani@live.in>
2016-02-16 22:06:54 -0500

    privatized chat

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+  var express = require('express');
+  var app = express();
+  var http = require('http').Server(app);
+  var io = require('socket.io')(http);
+  var _ = require('lodash-node');
 
-app.get('/', function(req, res){
-  res.sendfile('home.html');
-});
+  app.get('/', function(req, res){
+    res.sendfile('index.html');
+  });
 
-app.use('/js', express.static('public'));
-var clients = {};
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected'+socket.id);
-  });
-   socket.on('chat message', function(msg){
-    var jsonToSend = {
-      user : clients[socket.id],
-      message : msg
-    }
-    io.emit('chat message', JSON.stringify(jsonToSend) );
-  });
+  app.use('/js', express.static('public'));
+  var clients = {};
+  var sockets = {};
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    
+    socket.emit('connId',socket.id);
+    
+    sockets[socket.id] = socket;
+    
+    socket.on('disconnect', function(){
+      delete clients[socket.id];
+      delete sockets[socket.id];
+      io.emit('uname', JSON.stringify(clients));
+      console.log('user disconnected'+socket.id);
+    });
+
+    socket.on('chat message', function(jsonRecieved){
+      console.log(jsonRecieved);
+      jsonRecieved = JSON.parse(jsonRecieved);
+      var jsonToSend = {
+        user : clients[socket.id],
+        message : jsonRecieved.msg
+      };
+      sockets[jsonRecieved.to].emit('chat message', JSON.stringify(jsonToSend) );
+      sockets[jsonRecieved.from].emit('chat message', JSON.stringify(jsonToSend) );
+      
+    });
+
     socket.on('uname', function(uname){
-    console.log('message: ' + uname);
-    clients[socket.id] = uname;
-    io.emit('uname', 'User '+ uname + ' has logged in!');
-  });
-});
+      clients[socket.id] = uname;
+      io.emit('uname', JSON.stringify(clients));
+    });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
-});
+    socket.on('chatDest',function(val){
+      sockets[val].emit('chatReq',socket.id);
+    });
+
+  });
+
+  http.listen(process.env.PORT || 3000, function(){
+    console.log('listening on *:3000');
+  });
